457735cf3b89bba3a30028987c9e874d9682db66
PiterGrumpy <1hiaw.munozreynes.pedro@gmail.com>
2026-02-26 03:18:31 +0100

    Corregido tamaño de la letra

--- src/components/FilterGroup.jsx
+++ src/components/FilterGroup.jsx
@@ -13,7 +13,7 @@ export default function FilterGroup({ title, options, selected, setSelected }) {
             <div className="my-3">
                 <label className="text-xs uppercase font-bold tracking-widest text-stone-400 opacity-80 mb-4 block">{title}</label>
                 <div className="grid grid-cols-4 gap-2">
-                    <button className={`flex-1 py-2 px-2 rounded-xl text-sm font-bold transition-all shadow-lg shadow-amber-800/20 
+                    <button className={`flex-1 py-2 px-2 rounded-xl text-xs sm:text-sm md:text-base transition-all shadow-lg shadow-amber-800/20 
                                        ${selected.length === 0 ? "bg-amber-800 text-white" : "bg-stone-50 text-stone-400 hover:bg-stone-100"}`
                     }
                         onClick={() => setSelected([])}
@@ -22,7 +22,7 @@ export default function FilterGroup({ title, options, selected, setSelected }) {
                         <button
                             key={option}
                             onClick={() => handleClick(option)}
-                            className={`flex-1 py-2 px-2 rounded-xl text-sm font-bold transition-all shadow-lg shadow-amber-800/20 
+                            className={`flex-1 py-2 px-2 rounded-xl text-xs sm:text-sm md:text-base transition-all shadow-lg shadow-amber-800/20 
                                        ${selected.includes(option) ? "bg-amber-800 text-white" : "bg-stone-50 text-stone-400 hover:bg-stone-100"}`
                             }>
                             {option}
