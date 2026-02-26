1f79bf73af9a806dabd250f54bf6ec805ebef06e
PiterGrumpy <1hiaw.munozreynes.pedro@gmail.com>
2026-02-26 03:34:13 +0100

    Modificado el grid para moviles

--- src/components/FilterGroup.jsx
+++ src/components/FilterGroup.jsx
@@ -12,7 +12,7 @@ export default function FilterGroup({ title, options, selected, setSelected }) {
         <>
             <div className="my-3">
                 <label className="text-xs uppercase font-bold tracking-widest text-stone-400 opacity-80 mb-4 block">{title}</label>
-                <div className="grid grid-cols-4 gap-2">
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2">
                     <button className={`flex-1 py-2 px-2 rounded-xl text-xs sm:text-sm md:text-base transition-all shadow-lg shadow-amber-800/20 
                                        ${selected.length === 0 ? "bg-amber-800 text-white" : "bg-stone-50 text-stone-400 hover:bg-stone-100"}`
                     }
@@ -22,7 +22,7 @@ export default function FilterGroup({ title, options, selected, setSelected }) {
                         <button
                             key={option}
                             onClick={() => handleClick(option)}
-                            className={`py-2 px-3 rounded-xl text-xs sm:text-sm md:text-base transition-all shadow-lg shadow-amber-800/20 
+                            className={`flex-1 py-2 px-2 rounded-xl text-xs sm:text-sm md:text-base transition-all shadow-lg shadow-amber-800/20 
                                        ${selected.includes(option) ? "bg-amber-800 text-white" : "bg-stone-50 text-stone-400 hover:bg-stone-100"}`
                             }>
                             {option}
